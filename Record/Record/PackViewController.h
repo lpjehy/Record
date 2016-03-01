3c405ef91bba16fe83f7a0d2beefbd83eb3c1df4
Jehy <lpjehy@foxmail.com>
2016-03-01 21:55:35 +0800

    no message

---
+++ Record/Record/PackViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Record
+//
+//  Created by Jehy Fan on 16/3/1.
+//  Copyright © 2016年 Jehy Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PackViewController : UIViewController
+
+
+@end
+
---
+++ Record/Record/PackViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Record
+//
+//  Created by Jehy Fan on 16/3/1.
+//  Copyright © 2016年 Jehy Fan. All rights reserved.
+//
+
+#import "PackViewController.h"
+
+@interface PackViewController ()
+
+@end
+
+@implementation PackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
